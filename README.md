15693afc25ea4b02e1ba991f4ae5bb356c7f40b4
Melvynx <56388157+Melvynx@users.noreply.github.com>
2023-02-05 10:54:53 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextreact-stackblitz-nextjs-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/next-typescript-ndjt9j)
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex flex-col items-center gap-4 m-4">
+      <h1 className="text-4xl">NextReact</h1>
+      <p>Template</p>
+    </div>
+  );
+};
+
+export default Home;
